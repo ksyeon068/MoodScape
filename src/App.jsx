567f749580a28f3b95a1e4f613e44146b4db0472
seyoung <verbalyoung@gmail.com>
2026-03-04 17:07:16 +0900

    seyoung

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Section2 from "./components/Section2";
 import './App.css'
 
 function App() {
@@ -8,15 +9,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      
+      <Section2 />
     </>
   )
 }
