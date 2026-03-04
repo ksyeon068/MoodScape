35aeaee348dcb85e497872ed5db8d9c5b097fe9a
김세연 <ksyeon068@gmail.com>
2026-03-04 15:31:23 +0900

    기본틀

---
+++ src/components/Section2.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Section2 = () => {
+    return (
+        <div>
+            섹션2
+        </div>
+    );
+};
+
+export default Section2;
---
+++ src/components/Section4.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Section4 = () => {
+    return (
+        <div>
+            섹션4
+        </div>
+    );
+};
+
+export default Section4;
