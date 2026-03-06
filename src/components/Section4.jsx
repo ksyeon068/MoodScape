6c779537dfb54c6c33355b6eb01a356b2fd3b4c8
seongeun <seongeun040624@gmail.com>
2026-03-06 14:21:22 +0900

    5

--- src/components/Section4.jsx
+++ src/components/Section4.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import {useEffect} from "react";
+import { useApi } from "../context/ApiContext";
 import "../style/section4.scss";
 
 const Section4 = () => {
+	const { WEATHER_API_KEY } = useApi();
+
+	useEffect(() => {
+
+		const handleScroll = () => {
+
+			const elements = document.querySelectorAll(
+				".text, .circle, .top_des" // 수정: scroll-content > div 제거
+			);
+
+			const trigger = window.innerHeight * 0.75;
+
+			elements.forEach((el) => {
+
+				const rect = el.getBoundingClientRect();
+
+				if (rect.top < trigger && rect.bottom > 0) {
+					el.classList.add("fade-in");
+					el.classList.remove("fade-out");
+				} else {
+					el.classList.remove("fade-in");
+					el.classList.add("fade-out");
+				}
+
+			});
+		};
+
+		const scrollListener = () => {
+			requestAnimationFrame(handleScroll);
+		};
+
+		setTimeout(() => {
+			handleScroll();
+		}, 100);
+
+		window.addEventListener("scroll", scrollListener);
+
+		return () => {
+			window.removeEventListener("scroll", scrollListener);
+		};
+
+	}, []);
 
   return (
 		<section className="about">
@@ -22,13 +65,13 @@ const Section4 = () => {
 				<div className="scroll-content">
 
 					{/* left text 1 */}
-					<div className="left-content text1">
+					<div className="left-content text1 text">
 						<h3>변화하는 환경 속에서</h3>
 						<p>날씨는 <br />우리 일상에 <br />꾸준히 영향을 미치는 요소입니다.</p>
 					</div>
 
 					{/* right text 1 */}
-					<div className="right-content text2">
+					<div className="right-content text2 text">
 						<p>
 							하루의 기분, <br />활동 방식, <br />집중과 휴식의 흐름까지 <br />
 							날씨는 자연스럽게 삶의 리듬을 만들어 냅니다.
@@ -36,7 +79,7 @@ const Section4 = () => {
 					</div>
 
 					{/* left text 2 */}
-					<div className="left-content text3">
+					<div className="left-content text3 text">
 						<p>
 							이처럼 날씨는 단순한 자연 현상을 넘어 <br />
 							우리의 감정과 분위기에 깊게 연결되어 있습니다.
@@ -44,7 +87,7 @@ const Section4 = () => {
 					</div>
 
 					{/* left text 3 */}
-					<div className="right-content text4">
+					<div className="right-content text4 text">
 						<p>
 							<span>MoodScape</span>는 <br />
 							날씨의 변화를 바탕으로<br />
@@ -53,7 +96,7 @@ const Section4 = () => {
 					</div>
 
 					{/* right text 2 */}
-					<div className="right-content text5">
+					<div className="right-content text5 text">
 						<p>
 							단순히 음악을 나열하는 것이 아닌, <br />
 							하루의 분위기를 결정하는 공간입니다.
@@ -61,7 +104,7 @@ const Section4 = () => {
 					</div>
 
 					{/* right text 2 */}
-					<div className="left-content text6">
+					<div className="left-content text6 text">
 						<p>
 							날씨가 만들어 내는 분위기와 음악이 만나<br />
 							일상 속 작은 순간들을 <br />
@@ -69,37 +112,64 @@ const Section4 = () => {
 						</p>
 					</div>
 
-					<div className="bottom-text text7">
+					<div className="bottom-text text7 text">
 						<p>당신의 환경에 가장 어울리는 <br />음악을 발견하고, 경험해 보세요.</p>
 					</div>
 
 				</div>
 
-				{/* image circles */}
+			</div>
+
+			{/* image circles */}
+			<div className="decoCircles">
 				<div className="circle circle1">
-					<img src="../img/source/Weather_Pic 1.jpg" alt="" />
+					<img src="../img/source/Weather_Pic 4.jpg" alt="subWeatherImg1" />
 				</div>
 
 				<div className="circle circle2">
-					<img src="../img/source//Weather_Pic 3.jpg" alt="" />
+					<img src="../img/source//Weather_Pic 3.jpg" alt="subWeatherImg2" />
 				</div>
 
 				<div className="circle circle3">
-					<img src="../img/source//Weather_Pic 5.jpg" alt="" />
+					<img src="../img/source/Weather_Pic 5.jpg" alt="subWeatherImg3" />
 				</div>
 
 				<div className="circle circle4">
-					<img src="../img/source//Weather_Pic 7.jpg" alt="" />
+					<img src="../img/source/Weather_Pic 7.jpg" alt="subWeatherImg4" />
 				</div>
 
-				{/* decorative circles */}
-				<div className="circle deco1"></div>
-				<div className="circle deco2"></div>
-				<div className="circle deco3"></div>
+				<div className="circle circle5">
+					<img src="../img/source/Weather_Pic 11.jpg" alt="subWeatherImg" />
+				</div>
+				<div className="circle circle6">
+					<img src="../img/source/Weather_Pic 10.jpg" alt="subWeatherImg" />
+				</div>
+				<div className="circle circle7">
+					<img src="../img/source/Weather_Pic 2.jpg" alt="subWeatherImg" />
+				</div>
+				<div className="circle circle8">
+					<img src="../img/source/Weather_Pic 6.jpg" alt="subWeatherImg" />
+				</div>
+			</div>
 
-		</div>
-
-		
+			<div className="aboutLast">
+				<div className="lastTextBox">
+					<div className="texts">
+						<h2>MOODSCAPE</h2>
+						<p>	실시간 기상 데이터를 분석하여 상황에 적합한 사운드를 연결하고<br />
+							사용자가 별도의 탐색 없이도 자연스럽게 음악을 선택할 수 있도록 돕습니다.<br />
+							날씨와 음악을 하나의 흐름으로 연결한 새로운 방식의 추천 서비스<br />
+						</p>
+					</div>
+				</div>
+				<div className="aboutImgBox">
+					<div className="aboutBg"></div>
+					<img src="../img/source/about_b 1.jpg" alt="imgBox1" />
+					<img src="../img/source/about_b 2.jpg" alt="imgBox2" />
+					<img src="../img/source/about_b 3.jpg" alt="imgBox3" />
+					<img src="../img/source/about_b 4.jpg" alt="imgBox4" />
+				</div>
+			</div>
 		</section>
  	 );
 };
