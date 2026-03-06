7559f7f00c633a11f5a90148e1bf8e9b93ad1cae
seongeun <seongeun040624@gmail.com>
2026-03-06 10:28:17 +0900

    4

--- src/components/Section4.jsx
+++ src/components/Section4.jsx
@@ -24,43 +24,53 @@ const Section4 = () => {
 					{/* left text 1 */}
 					<div className="left-content text1">
 						<h3>변화하는 환경 속에서</h3>
+						<p>날씨는 <br />우리 일상에 <br />꾸준히 영향을 미치는 요소입니다.</p>
 					</div>
 
 					{/* right text 1 */}
 					<div className="right-content text2">
 						<p>
-							날씨는 <br />
-							우리의 일상에 <br />
-							꾸준히 영향을 미치는 요소입니다.
+							하루의 기분, <br />활동 방식, <br />집중과 휴식의 흐름까지 <br />
+							날씨는 자연스럽게 삶의 리듬을 만들어 냅니다.
 						</p>
 					</div>
 
 					{/* left text 2 */}
 					<div className="left-content text3">
 						<p>
-							하루의 분위기, 활동 방식 <br />
-							집중도와 휴식의 흐름까지 —
+							이처럼 날씨는 단순한 자연 현상을 넘어 <br />
+							우리의 감정과 분위기에 깊게 연결되어 있습니다.
 						</p>
 					</div>
 
 					{/* left text 3 */}
-					<div className="left-content text4">
+					<div className="right-content text4">
 						<p>
-							날씨는 생활의 리듬을 <br />
-							자연스럽게 만들어냅니다.
+							<span>MoodScape</span>는 <br />
+							날씨의 변화를 바탕으로<br />
+							순간의 분위기에 어울리는 음악을 추천합니다.
 						</p>
 					</div>
 
 					{/* right text 2 */}
 					<div className="right-content text5">
 						<p>
-							<span>MoodScape</span>는 오늘의 날씨 정보를 기반으로 <br />
-							그 순간의 분위기에 어울리는 음악을 추천합니다.
+							단순히 음악을 나열하는 것이 아닌, <br />
+							하루의 분위기를 결정하는 공간입니다.
 						</p>
 					</div>
 
-					<div className="bottom-text text6">
-						<p>당신의 환경에 가장 어울리는 소리를 경험해 보세요.</p>
+					{/* right text 2 */}
+					<div className="left-content text6">
+						<p>
+							날씨가 만들어 내는 분위기와 음악이 만나<br />
+							일상 속 작은 순간들을 <br />
+							더욱 풍부하게 만들어 줍니다.
+						</p>
+					</div>
+
+					<div className="bottom-text text7">
+						<p>당신의 환경에 가장 어울리는 <br />음악을 발견하고, 경험해 보세요.</p>
 					</div>
 
 				</div>
@@ -89,6 +99,7 @@ const Section4 = () => {
 
 		</div>
 
+		
 		</section>
  	 );
 };
