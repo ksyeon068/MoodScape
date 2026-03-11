ceee733bdd371e56ff49883cf328c37861d92091
김세연 <ksyeon068@gmail.com>
2026-03-11 17:55:37 +0900

    ajtlrltnwjd

--- src/App.jsx
+++ src/App.jsx
@@ -6,40 +6,49 @@ import Section2 from './components/Section2.jsx'
 import Section3 from './components/Section3.jsx'
 import Section4 from './components/Section4.jsx'
 import Footer from './components/Footer.jsx'
-import Loading from './components/loader/Loader.jsx'
+import Loader from './components/loader/Loader.jsx'
 
-import { ApiProvider } from './context/ApiContext'; // Provider 불러오기
+import { ApiProvider, useApi } from './context/ApiContext'
 import './App.css'
 
+function AppContent() {
+
+  const { loading } = useApi();
+
+  if (loading) return <Loader />;
+
+  return (
+    <div className="App">
+      <Header />
+      <Bg />
+
+      <section id="home">
+        <Section1 />
+      </section>
+
+      <section id="today">
+        <Section2 />
+      </section>
+
+      <section id="weather">
+        <Section3 />
+      </section>
+
+      <section id="about">
+        <Section4 />
+      </section>
+
+      <Footer />
+    </div>
+  );
+}
+
 function App() {
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 3000); // 3초 후 로딩 끝
-  }, []);
-  if (loading) return <Loading />;
   return (
     <ApiProvider>
-      <div className="App">
-        <Header />
-        <Bg />
-        <section id="home">
-          <Section1 />
-        </section>
-        <section id="today">
-          <Section2 />
-        </section>
-        <section id="weather">
-          <Section3 />
-        </section>
-        <section id="about">
-          <Section4 />
-        </section>
-        <Footer />
-      </div>
+      <AppContent />
     </ApiProvider>
-  )
+  );
 }
 
 export default App
